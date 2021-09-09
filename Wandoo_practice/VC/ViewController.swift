497c5b449ebdbf82df57533760faba5497ce23a2
hyerin <hope1053@naver.com>
2021-09-09 13:43:17 +0900

    [210909] 다크모드 대응 업데이트

--- Wandoo_practice/VC/ViewController.swift
+++ Wandoo_practice/VC/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         viewModel.loadTasks()
         showAddLable()
